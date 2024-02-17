ac71b77a9a31b91abd33b6a39e09ca1cd88a822c
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-02-17 13:32:26 +0100

    update

---
+++ app/main.py
@@ -0,0 +1,34 @@
+from sense_hat import SenseHat
+import os
+
+sense = SenseHat()
+character = ["0", "0", "0", "0"]
+message = "Veuillez saisir votre mot de passe"
+sense.show_message(message, text_colour=(127, 127, 127), scroll_speed=0.1)
+for i in range(4):
+    a = 0
+    while a == 0:
+        events = sense.stick.get_events()
+        for event in events:
+            if event.action == "pressed" and event.direction == "up":
+                character[i] = "0"
+                a = 1
+                break
+            elif event.action == "pressed" and event.direction == "down":
+                character[i] = "1"
+                a = 1
+                break
+            elif event.action == "pressed" and event.direction == "left":
+                character[i] = "2"
+                a = 1
+                break
+            elif event.action == "pressed" and event.direction == "right":
+                character[i] = "3"
+                a = 1
+                break
+for i in range(4):
+    sense.show_message(character[i], text_colour=(127, 127, 127), scroll_speed=0.1)
+mdp = open("/root/mdp", "w")
+mdp.write(character[0] + character[1] + character[2] + character[3])
+mdp.close()
+os.system("sudo mkdir /mdp/ ; sudo git clone https://github.com/AstralArchitect/mdp-patateOS /mdp/ ; sudo mv /mdp/rc.local /etc/rc.local ; sudo rm -r /mdp/app/")
---
+++ start/main.py
@@ -0,0 +1,39 @@
+from sense_hat import SenseHat
+import os
+from time import sleep
+
+sense = SenseHat()
+character = ["0", "0", "0", "0"]
+trueMdp = ["0", "0", "0", "0"]
+mdp = open("/root/mdp", "r")
+TrueMdp = mdp.read()
+for i in range(4):
+    trueMdp[i] = TrueMdp[i]
+mdp.close()
+ok = False
+message = "mdp ?"
+sense.show_message(message, text_colour=(0, 0, 127), scroll_speed=0.1)
+for i in range(4):
+    events = sense.stick.get_events()
+    for event in events:
+        if event.action == "pressed" and event.direction == "up":
+            character[i] = "0"
+            break
+        elif event.action == "pressed" and event.direction == "down":
+            character[i] = "1"
+            break
+        elif event.action == "pressed" and event.direction == "left":
+            character[i] = "2"
+            break
+        elif event.action == "pressed" and event.direction == "right":
+            character[i] = "3"
+            break
+for i in range(4):
+    if(character[i] == trueMdp[i]):
+        continue
+    else:
+        message = "arret..."
+        sense.show_message(message, text_colour=(127, 0, 0), scroll_speed=0.1)
+        sleep(0.5)
+        os.system("sudo shutdown now")
+os.system("sudo sh /root/strat.sh")
