7a7aa6f31adbfc6422da6ba78a032650473341a9
AstralArchitect <matthieu.jlp.poncet@gmail.com>
2024-02-17 13:59:44 +0100

    update

--- start/main.py
+++ start/main.py
@@ -14,20 +14,26 @@ ok = False
 message = "mdp ?"
 sense.show_message(message, text_colour=(0, 0, 127), scroll_speed=0.1)
 for i in range(4):
-    events = sense.stick.get_events()
-    for event in events:
-        if event.action == "pressed" and event.direction == "up":
-            character[i] = "0"
-            break
-        elif event.action == "pressed" and event.direction == "down":
-            character[i] = "1"
-            break
-        elif event.action == "pressed" and event.direction == "left":
-            character[i] = "2"
-            break
-        elif event.action == "pressed" and event.direction == "right":
-            character[i] = "3"
-            break
+    a = 0
+    while a == 0:
+        events = sense.stick.get_events()
+        for event in events:
+            if event.action == "pressed" and event.direction == "up":
+                character[i] = "0"
+                a = 1
+                break
+            elif event.action == "pressed" and event.direction == "down":
+                character[i] = "1"
+                a = 1
+                break
+            elif event.action == "pressed" and event.direction == "left":
+                character[i] = "2"
+                a = 1
+                break
+            elif event.action == "pressed" and event.direction == "right":
+                character[i] = "3"
+                a = 1
+                break
 for i in range(4):
     if(character[i] == trueMdp[i]):
         continue
